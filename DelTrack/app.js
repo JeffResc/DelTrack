50394723334b3b0cc913148edb8d01957d6ebddb
Jeff Rescignano <jeff@jeffresc.dev>
2021-11-29 15:47:35 -0500

    Update app.js

--- DelTrack/app.js
+++ DelTrack/app.js
@@ -126,7 +126,7 @@ app.post('/add_bulk_shipment', functions.ensureLogin, (req, res) => {
 
 app.post('/track_and_update', functions.ensureLogin, (req, res) => {
     functions.trackAndUpdate(req.body.courier, req.body.tracking_id);
-    res.send(JSON.stringify({ msg: 'Successfully updated ' + req.body.courier_code + ' ' + req.body.tracking_id }));
+    res.send(JSON.stringify({ msg: 'Successfully updated ' + req.body.courier + ' ' + req.body.tracking_id }));
 });
 
 app.post('/submit_settings', functions.ensureLogin, (req, res) => {
